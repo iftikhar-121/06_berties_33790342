a16a1f642202d212cdef90fc8bb11c4ff80ddff1
iftikharhasan7@gmail.com <iftikharhasan7@gmail.com>
2025-11-20 18:05:16 +0000

    task 2

--- create_db.sql
+++ create_db.sql
@@ -13,6 +13,15 @@ CREATE TABLE IF NOT EXISTS books (
     price  DECIMAL(5, 2) UNSIGNED,
     PRIMARY KEY(id));
 
+CREATE TABLE IF NOT EXISTS users (
+    user_id          INT AUTO_INCREMENT,
+    username         VARCHAR(50) UNIQUE NOT NULL,
+    first_name       VARCHAR(50) NOT NULL,
+    last_name        VARCHAR(50) NOT NULL,
+    email            VARCHAR(100) NOT NULL,
+    hashed_password  VARCHAR(100) NOT NULL, 
+    PRIMARY KEY(user_id)
+);
 # Create the application user
 CREATE USER IF NOT EXISTS 'berties_books_app'@'localhost' IDENTIFIED BY 'qwertyuiop'; 
 GRANT ALL PRIVILEGES ON berties_books.* TO 'berties_books_app'@'localhost';
--- routes/users.js
+++ routes/users.js
@@ -2,13 +2,43 @@
 const express = require("express")
 const router = express.Router()
 
+// 1. IMPORT BCRYPT
+const bcrypt = require('bcrypt') 
+const saltRounds = 10 
+
 router.get('/register', function (req, res, next) {
     res.render('register.ejs')
 })
 
 router.post('/registered', function (req, res, next) {
-    // saving data in database
-    res.send(' Hello '+ req.body.first + ' '+ req.body.last +' you are now registered!  We will send an email to you at ' + req.body.email);                                                                              
+    
+    // 2. CAPTURE PLAIN PASSWORD
+    const plainPassword = req.body.password 
+
+    // 3. HASH THE PASSWORD AND DEFINE THE INSERT LOGIC INSIDE THE CALLBACK
+    bcrypt.hash(plainPassword, saltRounds, function(err, hashedPassword) { 
+        
+        if (err) {
+            return next(err); // Handle hashing error
+        }
+
+        // 4. DATABASE INSERT (Refer to /books/bookadded route for a hint [cite: 356])
+        let sqlquery = "INSERT INTO users (username, first_name, last_name, email, hashed_password) VALUES (?,?,?,?,?)";
+        let newrecord = [req.body.username, req.body.first, req.body.last, req.body.email, hashedPassword];
+
+        db.query(sqlquery, newrecord, (db_err, result) => {
+            if (db_err) {
+                return next(db_err); // Handle database insertion error
+            }
+
+            // 5. SEND DEBUGGING MESSAGE
+            let result_message = 'Hello '+ req.body.first + ' '+ req.body.last +' you are now registered! '
+            result_message += 'We will send an email to you at ' + req.body.email
+            result_message += 'Your password is: '+ req.body.password +' and your hashed password is: '+ hashedPassword 
+            res.send(result_message) 
+        }); // end db.query
+
+    }) // end bcrypt.hash
 }); 
 
 // Export the router object so index.js can access it
